ba2e150d6cf478793f0203cc5866709e5cc0c720
Jiří Svítil <svitijir@fel.cvut.cz>
2023-02-16 21:54:58 +0100

    done comparing antennnas

--- unnamed-antenna-site/src/app/compare/compare.component.ts
+++ unnamed-antenna-site/src/app/compare/compare.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IContentAntenna, IFrequencyTable } from '../model/chart';
+import { ICompatibleAntenna, IContentAntenna, IFrequencyTable } from '../model/chart';
 import { CompareService } from '../services/compare.service';
 import { FileloadingService } from '../services/fileloading.service';
 import { ListingService } from '../services/listing.service';
@@ -8,6 +8,7 @@ import {
   AnalogueFrequencyTable,
   DigFrequencyTable,
 } from '../data/frequency-table';
+import { StoreService } from '../services/store.service';
 
 @Component({
   selector: 'app-compare',
@@ -16,11 +17,12 @@ import {
 })
 export class CompareComponent implements OnInit {
   ready: boolean = false;
+  compatibleReady: boolean = false;
   antennaList: IContentAntenna[] = [];
   results = [];
   optionsList: { id: string; name: string }[] = [];
   selected: { id: string; name: string }[] = [];
-
+  compatibleAntennaList: ICompatibleAntenna[] = [];
   methodsList: { id: number; name: string }[] = [
     { id: 0, name: 'Find common frequencies' },
     { id: 1, name: 'Min data (useful for diversity systems)' },
@@ -28,7 +30,7 @@ export class CompareComponent implements OnInit {
   ];
 
   methods = [this.files.maxData, this.files.minData, this.files.averageData];
-  
+
   selectedMethod: { id: number; name: string } = this.methodsList[0];
 
   tablesList: { id: number; name: string }[] = [
@@ -37,15 +39,20 @@ export class CompareComponent implements OnInit {
   ];
 
   tables: IFrequencyTable[][] = [AnalogueFrequencyTable, DigFrequencyTable];
-  
+
   selectedTables: { id: number; name: string }[] = [];
 
   get isReady(): boolean {
     return this.ready;
   }
+  
+  get isCompatibleReady(): boolean {
+    return this.compatibleReady;
+  }
 
   constructor(
     private listing: ListingService,
+    private store: StoreService,
     public compare: CompareService,
     public files: FileloadingService
   ) {}
@@ -59,5 +66,31 @@ export class CompareComponent implements OnInit {
 
     this.ready = true;
   }
-  
+
+  private getTables(): IFrequencyTable[][] {
+    let result: IFrequencyTable[][] = [];
+
+    this.selectedTables.forEach((table) => {
+      result.push(this.tables[table.id]);
+    });
+
+    return result;
+  }
+
+  public async findAntennas() {
+    let tables = this.getTables();
+    if (tables.length == 0) {
+      alert('Please select bands of interest');
+      return;
+    }
+
+    this.compare.findFrequencies(
+      this.selected,
+      this.antennaList,
+      this.methods[this.selectedMethod.id]
+    );
+    if (!this.store.getAntenna().hasOwnProperty('id') || this.store.getFrequencyData().length == 0) return;
+    this.compatibleAntennaList = await this.compare.getListOfCompatibleAntennas(this.store.getAntenna(), this.store.getFrequencyData(), tables, 2);
+    this.compatibleReady = true;
+  }
 }
